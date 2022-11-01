64f7d8e09922024711a79ab7b0e2b15bef2ce87d
Evan Curtis <ewcurtis1997@gmail.com>
2022-11-01 16:20:12 -0400

    Add basic functionality to display task dom by clicking a button in the project dashboard

---
+++ src/TaskDom.js
@@ -0,0 +1,115 @@
+//Makes changes to the DOM in order to display the create/edit task screen
+
+class TaskDom {
+    createTask() {
+    //New Task Dashboard
+    const newTask = document.createElement("div");
+    newTask.setAttribute("class", "new-task-card");
+
+    const taskTitle = document.createElement("p");
+    taskTitle.textContent = "New Task";
+    newTask.appendChild(taskTitle);
+
+    //Task Name
+    const taskNameField = document.createElement("div");
+    const taskNameLabel = document.createElement("label");
+    taskNameLabel.textContent = "Task Name: ";
+    taskNameLabel.setAttribute("for", "task-name");
+    taskNameField.appendChild(taskNameLabel);
+
+    const taskNameInput = document.createElement("input");
+    taskNameInput.setAttribute("id", "task-name");
+    taskNameInput.setAttribute("placeholder", "Max 25 chars");
+    taskNameInput.setAttribute("maxlength", "25");
+    taskNameField.appendChild(taskNameInput);
+    newTask.appendChild(taskNameField);
+
+    //Task Due Date
+    const taskDueDateField = document.createElement("div");
+    const taskDueDateLabel = document.createElement("label");
+    taskDueDateLabel.textContent = "Due Date: ";
+    taskDueDateLabel.setAttribute("for", "task-due-date");
+    taskDueDateField.appendChild(taskDueDateLabel);
+
+    const taskDueDateInput = document.createElement("input");
+    taskDueDateInput.setAttribute("id", "task-due-date");
+    taskDueDateInput.setAttribute("type", "date");
+    taskDueDateField.appendChild(taskDueDateInput);
+    newTask.appendChild(taskDueDateField);
+
+    //Task Description
+    const taskDescField = document.createElement("div");
+    taskDescField.setAttribute("class", "text-area");
+    const taskDescLabel = document.createElement("label");
+    taskDescLabel.textContent = "Task Description: ";
+    taskDescLabel.setAttribute("for", "task-desc");
+    taskDescField.appendChild(taskDescLabel);
+
+    const taskDescInput = document.createElement("textArea");
+    taskDescInput.setAttribute("id", "task-desc");
+    taskDescField.appendChild(taskDescInput);
+    newTask.appendChild(taskDescField);
+
+    //Priorities
+    const priorities = document.createElement("div");
+    priorities.setAttribute("id", "priorities");
+    
+    //Low
+    const low = document.createElement("input");
+    low.setAttribute("type", "radio");
+    low.setAttribute("checked", "true");
+    low.setAttribute("id", "low");
+    low.setAttribute("name", "p");
+    priorities.appendChild(low);
+
+    const lowLabel = document.createElement("label");
+    lowLabel.setAttribute("for", "low");
+    lowLabel.textContent = "Low";
+    priorities.appendChild(lowLabel);
+
+    //Medium
+    const med = document.createElement("input");
+    med.setAttribute("type", "radio");
+    med.setAttribute("id", "medium");
+    med.setAttribute("name", "p");
+    priorities.appendChild(med);
+
+    const medLabel = document.createElement("label");
+    medLabel.setAttribute("for", "medium");
+    medLabel.textContent = "Medium";
+    priorities.appendChild(medLabel);
+
+    //High
+    const high = document.createElement("input");
+    high.setAttribute("type", "radio");
+    high.setAttribute("id", "high");
+    high.setAttribute("name", "p");
+    priorities.appendChild(high);
+
+    const highLabel = document.createElement("label");
+    highLabel.setAttribute("for", "high");
+    highLabel.textContent = "High";
+    priorities.appendChild(highLabel);
+
+    newTask.appendChild(priorities);
+
+    //Task Buttons
+    const taskButtons = document.createElement("div");
+    taskButtons.setAttribute("class", "new-buttons");
+    const newTaskButton = document.createElement("button");
+    newTaskButton.setAttribute("class", "confirm");
+    newTaskButton.textContent = "Create Project";
+    taskButtons.appendChild(newTaskButton);
+
+    const cancelTask = document.createElement("button");
+    cancelTask.setAttribute("class", "cancel");
+    cancelTask.textContent = "Cancel";
+    taskButtons.appendChild(cancelTask);
+
+    newTask.appendChild(taskButtons);
+
+    return newTask;
+    }
+}
+
+export default TaskDom;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Menu from './menu.svg';
 import Edit from './text-box-edit-outline.png';
 import CPlus from './plus-circle.png';
 import Plus from './plus.png';
+import TaskDom from './TaskDom.js';
 
 
 function makeInbox(title, id) {
@@ -86,6 +87,14 @@ function webpage() {
 
     const cPlus = new Image();
     cPlus.src = CPlus;
+    cPlus.addEventListener("click", () => {
+        console.log("Got here!");
+        const task = new TaskDom();
+        while (main.firstChild) {
+            main.removeChild(main.firstChild);
+        }
+        main.appendChild(task.createTask());
+    })
     taskContainer.appendChild(cPlus);
 
     const taskCard = document.createElement("div");
@@ -157,118 +166,7 @@ function webpage() {
     projButtons.appendChild(cancelProj);
 
     newProj.appendChild(projButtons);
-
-
-
     main.appendChild(newProj);
-
-    //New Task Dashboard
-    const newTask = document.createElement("div");
-    newTask.setAttribute("class", "new-task-card");
-
-    const taskTitle = document.createElement("p");
-    taskTitle.textContent = "New Task";
-    newTask.appendChild(taskTitle);
-
-    //Task Name
-    const taskNameField = document.createElement("div");
-    const taskNameLabel = document.createElement("label");
-    taskNameLabel.textContent = "Task Name: ";
-    taskNameLabel.setAttribute("for", "task-name");
-    taskNameField.appendChild(taskNameLabel);
-
-    const taskNameInput = document.createElement("input");
-    taskNameInput.setAttribute("id", "task-name");
-    taskNameInput.setAttribute("placeholder", "Max 25 chars");
-    taskNameInput.setAttribute("maxlength", "25");
-    taskNameField.appendChild(taskNameInput);
-    newTask.appendChild(taskNameField);
-
-    //Task Due Date
-    const taskDueDateField = document.createElement("div");
-    const taskDueDateLabel = document.createElement("label");
-    taskDueDateLabel.textContent = "Due Date: ";
-    taskDueDateLabel.setAttribute("for", "task-due-date");
-    taskDueDateField.appendChild(taskDueDateLabel);
-
-    const taskDueDateInput = document.createElement("input");
-    taskDueDateInput.setAttribute("id", "task-due-date");
-    taskDueDateInput.setAttribute("type", "date");
-    taskDueDateField.appendChild(taskDueDateInput);
-    newTask.appendChild(taskDueDateField);
-
-    //Task Description
-    const taskDescField = document.createElement("div");
-    taskDescField.setAttribute("class", "text-area");
-    const taskDescLabel = document.createElement("label");
-    taskDescLabel.textContent = "Task Description: ";
-    taskDescLabel.setAttribute("for", "task-desc");
-    taskDescField.appendChild(taskDescLabel);
-
-    const taskDescInput = document.createElement("textArea");
-    taskDescInput.setAttribute("id", "task-desc");
-    taskDescField.appendChild(taskDescInput);
-    newTask.appendChild(taskDescField);
-
-    //Priorities
-    const priorities = document.createElement("div");
-    priorities.setAttribute("id", "priorities");
-    
-    //Low
-    const low = document.createElement("input");
-    low.setAttribute("type", "radio");
-    low.setAttribute("checked", "true");
-    low.setAttribute("id", "low");
-    low.setAttribute("name", "p");
-    priorities.appendChild(low);
-
-    const lowLabel = document.createElement("label");
-    lowLabel.setAttribute("for", "low");
-    lowLabel.textContent = "Low";
-    priorities.appendChild(lowLabel);
-
-    //Medium
-    const med = document.createElement("input");
-    med.setAttribute("type", "radio");
-    med.setAttribute("id", "medium");
-    med.setAttribute("name", "p");
-    priorities.appendChild(med);
-
-    const medLabel = document.createElement("label");
-    medLabel.setAttribute("for", "medium");
-    medLabel.textContent = "Medium";
-    priorities.appendChild(medLabel);
-
-    //High
-    const high = document.createElement("input");
-    high.setAttribute("type", "radio");
-    high.setAttribute("id", "high");
-    high.setAttribute("name", "p");
-    priorities.appendChild(high);
-
-    const highLabel = document.createElement("label");
-    highLabel.setAttribute("for", "high");
-    highLabel.textContent = "High";
-    priorities.appendChild(highLabel);
-
-    newTask.appendChild(priorities);
-
-    //Task Buttons
-    const taskButtons = document.createElement("div");
-    taskButtons.setAttribute("class", "new-buttons");
-    const newTaskButton = document.createElement("button");
-    newTaskButton.setAttribute("class", "confirm");
-    newTaskButton.textContent = "Create Project";
-    taskButtons.appendChild(newTaskButton);
-
-    const cancelTask = document.createElement("button");
-    cancelTask.setAttribute("class", "cancel");
-    cancelTask.textContent = "Cancel";
-    taskButtons.appendChild(cancelTask);
-
-    newTask.appendChild(taskButtons);
-
-    main.appendChild(newTask);
 
 
     content.appendChild(header);
