11173fbe56c95bc103fa62516abee366803564a4
Evan Curtis <ewcurtis1997@gmail.com>
2022-11-03 15:37:14 -0400

    Add Project class for new projects

---
+++ src/Project.js
@@ -0,0 +1,9 @@
+class Project {
+    constructor(name, taskArray, favorited=false) {
+        this.name = name;
+        this.taskArray = taskArray;
+        this.favorited = favorited;
+    }
+}
+
+export default Project;
